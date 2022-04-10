6a036965ef564a6e1f5844a0764146c22d649fa2
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-10 12:14:24 -0400

    commit tree 0e419d32d21e8a285f885f741faf0cc87d80c29b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+const destructivelyAppendCat = (name) => cats.push(name);
+
+const destructivelyPrependCat = (name) => cats.unshift(name);
+
+const destructivelyRemoveLastCat = (name) => cats.pop(name);
+
+const destructivelyRemoveFirstCat = (name) => cats.shift(name);
+
+const appendCat = (name) => [...cats, name];
+
+const prependCat = (name) => [name, ...cats];
+
+const removeLastCat = () => cats.slice(0, -1);
+
+const removeFirstCat = () => cats.slice(1);
